a4d3c41bd58b5f120a59b4940c3b4dd79e2fbac5
bariot <156963074+bariot@users.noreply.github.com>
2024-01-28 17:34:40 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,460 @@
+var plot = [
+    {
+        "bgColor": "./assets/bcg_office2.jpg",
+        "leftImage": null,
+        "rightImage": null,
+        "speaker": null,
+        "text": "Зов Ктулху"
+    },
+    {
+        "bgColor": "./assets/bcg_office2.jpg",
+        "leftImage": null,
+        "rightImage": null,
+        "speaker": null,
+        "text": "Офис."
+    },
+    {
+        "bgColor": "./assets/bcg_office2.jpg",
+        "leftImage": "assets/an2.png",
+        "rightImage": null,
+        "speaker": "Настя",
+        "text": "Привет, как прошли выходные?"
+    },
+    {
+        "bgColor": "./assets/bcg_office2.jpg",
+        "leftImage": null,
+        "rightImage": "./assets/fr2.png",
+        "speaker": "Вика",
+        "text": "Привет. Просто ужасно. Все выходные пришлось заниматься этим договором. Я не..."
+    },
+    {
+        "bgColor": "./assets/bcg_office2.jpg",
+        "leftImage": null,
+        "rightImage": "./assets/s1.png",
+        "speaker": "Сергей(<i>улыбаясь</i>)",
+        "text": "Что за две роскошные тигрицы? Рядом в зоопарке опять забыли закрыть ворота?"
+    },
+    {
+        "bgColor": "./assets/bcg_office2.jpg",
+        "leftImage": null,
+        "rightImage": "./assets/s1.png",
+        "speaker": "Сергей(<i>флиртующе улыбаясь</i>)",
+        "text": "Настя, привет. Как всегда прекрасно вглядишь"
+    },
+    {
+        "bgColor": "./assets/bcg_office2.jpg",
+        "leftImage": "./assets/an2.png",
+        "rightImage": null,
+        "speaker": "Настя<i>(сухо)</i>",
+        "text": "Привет"
+    },
+    {
+        "bgColor": "./assets/bcg_office2.jpg",
+        "leftImage": "./assets/an2.png",
+        "rightImage": null,
+        "speaker": "Настя",
+        "text": "Господи, что за дурацкий подкат."
+    },
+    {
+        "bgColor": "./assets/bcg_office2.jpg",
+        "leftImage": "./assets/an2.png",
+        "rightImage": null,
+        "speaker": "Настя",
+        "text": "Какой же он настырный. Вот всë никак не уймется"
+    },
+    {
+        "bgColor": "./assets/bcg_office2.jpg",
+        "leftImage": null,
+        "rightImage": "./assets/fr2.png",
+        "speaker": "Вика",
+        "text": "А по-моему, мило. О я, конечно, не про подкат - это кошмар полный. А про знаки внимания. Ты ему явно нравишься и, он к тебе неровно дышит"
+    },
+    {
+        "bgColor": "./assets/portal.png",
+        "leftImage": null,
+        "rightImage": null,
+        "speaker": null,
+        "text": "Сразу после этой беседы, герои разошлись по своим рабочим местам. Внезапно, в центре офиса начало формироваться что-то странное. Портал, сияющий сверкающими огнями, открылся, и его энергия начала разрушать стены офисного пространства. Работники в испуге бросились врассыпную, пытаясь спастись от необъяснимой угрозы. Из портала вышли два страшных демона, испускающих серый туман. Их глаза сверкали зловещим светом, и в руках они держали огромные темные цепи. Все вокруг наполнилось мраком и холодом."
+    },
+    {
+        "bgColor": "./assets/portal.png",
+        "leftImage": "./assets/demon1.png",
+        "rightImage": null,
+        "speaker": "Демон1",
+        "text": "Бариот - убийца демонов, мы тебя нашли! Теперь ты сам к нам придешь, и мы отомстим тебе за наших сородичей."
+    },
+    {
+        "bgColor": "./assets/portal.png",
+        "leftImage": "./assets/demon1.png",
+        "rightImage": null,
+        "speaker": "Демон1",
+        "text": "Хватай ее. "
+    },
+    {
+        "bgColor": "./assets/portal.png",
+        "leftImage": "./assets/demon1.png",
+        "rightImage": null,
+        "speaker": "Демон1",
+        "text": "Он точно за ней явится!"
+    },
+    {
+        "bgColor": "./assets/portal.png",
+        "leftImage": null,
+        "rightImage": null,
+        "speaker": null,
+        "text": "Демоны направили свой взгляд на Настю, определенно выделяя ее среди всех присутствующих."
+    },
+    {
+        "bgColor": "./assets/portal.png",
+        "leftImage": null,
+        "rightImage": null,
+        "speaker": null,
+        "text": "Настя, едва понимая, что происходит, была взята в плен демонами. Она пыталась сопротивляться, но их темные цепи были слишком могущественными."
+    },
+    {
+        "bgColor": "./assets/portal.png",
+        "leftImage": "./assets/an2.png",
+        "rightImage": null,
+        "speaker": "Настя<i>(милым и дрожащим голосом)</i>",
+        "text": "Спасите"
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": "./assets/s1.png",
+        "speaker": "Сергей",
+        "text": "Они все-таки нашли меня и забрали Настю... Я должен ее спасти во что бы то ни стало..."
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": null,
+        "speaker": null,
+        "text": "Сергей, ощущая опасность, быстро реагирует и налагает магический щит на Настю, сдерживая попытки демонов унести её через портал."
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": "./assets/s1.png",
+        "speaker": "Сергей",
+        "text": "'Custos Vita!' (жизненный щит)"
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": null,
+        "speaker": null,
+        "text": "Этот магический барьер демоном не преодолеть и они озадаченно отступают. "
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": null,
+        "speaker": null,
+        "text": "Демоны, не смогшие преодолеть магический барьер, озадаченно отступают. Однако, пока Сергей осмеливается отдышаться, один из демонов удирает, утащив Настю через портал."
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": "./assets/s1.png",
+        "speaker": "Сергей <i>(с гневом)</i>",
+        "text": "Проклятие!"
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": null,
+        "speaker": null,
+        "text": "Вдруг портал начал терять энергию и быстро закрываться. Сергей, понимая, что не успеет в портал, призывает своего верного помощника-леприкона"
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": "./assets/s1.png",
+        "speaker": "Сергей",
+        "text": "Лепи, следи за ними, выясни, куда они унесли Настю. Верю в тебя, мой друг"
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": null,
+        "speaker": null,
+        "text": "Леприкон кивает, исчезая в вихре света, чтобы следовать за демонами и выяснить их намерения."
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": null,
+        "speaker": null,
+        "text": "Сергей, после того как портал окончательно закрылся, начал осматривать офис в поисках Вики. "
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": null,
+        "speaker": null,
+        "text": "Вика сидела в углу офиса, окруженная рарушенными балками здания"
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": "../assets/fr2.png",
+        "speaker": "Вика <i>(испугано)</i>",
+        "text": "Что происходит? Что это было?"
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": null,
+        "speaker": null,
+        "text": "Сергей, стоящий в разгромленном офисе, понимает, что ему ничего не остается, кроме как следовать за демонами в их мир. Он достает из кармана амулет, предназначенный для открытия порталов между мирами, и смотрит на него, словно подтверждая свое решение."
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": "./assets/s1.png",
+        "speaker": "Сергей <i>(с решимостью)</i>",
+        "text": "Настя, я не оставлю тебя в руках этих тварей. Мир демонов, держись."
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": "./assets/s1.png",
+        "speaker": "Сергей",
+        "text": "Нужно применить заклятие \"передеватус\", чтобы обличится в мой костюм для истребления демонов"
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": "./assets/s1_other.png",
+        "speaker": "Сергей",
+        "text": "Ой, не тот.."
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": "./assets/s1_otherGod.png",
+        "speaker": "Сергей",
+        "text": " нет, не та эпоха..."
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": "./assets/s1_mainlook.png",
+        "speaker": "Сергей",
+        "text": "Идеально."
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": null,
+        "speaker": null,
+        "text": "С уверенностью в своих силах, Сергей начинает читать заклинание, призывая портал. Энергия амулета начинает сверкать, создавая вокруг него магический вихрь. Внезапно, перед ним появляется новый портал, мерцающий неестественным светом."
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": null,
+        "speaker": null,
+        "text": "Сергей переступает порог портала, оставив разрушенный офис позади. Его окружает мрак и неизведанный мир демонов. Он чувствует, как атмосфера становится гуще, и темные тени начинают медленно подниматься."
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": "./assets/s1_mainlook.png",
+        "speaker": "Сергей <i>(мысленно)</i>",
+        "text": "Настя, я обязательно найду тебя."
+    },
+    {
+        "bgColor": "./assets/crash_office.png",
+        "leftImage": null,
+        "rightImage": null,
+        "speaker": null,
+        "text": "Портал закрывается, и Сергей исчезает в неизведанных просторах мира демонов, полный решимости спасти свою подругу и покончить с этим кошмаром."
+    },
+    {
+        "bgColor": "./assets/bcg_office2.jpg",
+        "leftImage": null,
+        "rightImage": null,
+        "speaker": null,
+        "text": "Продолжение уже скоро... Буду Благодарен за отзыв об игре. Можете поблагодарить разработчика теплыми словами)))"
+    },
+
+]
+
+
+let main = document.querySelector('.background')
+
+
+
+
+const start = function() {
+
+    var i = 0
+    render = `
+            <div class="dialog-box" id="dialogBox">
+                
+                <p id="dialogText">${plot[i]['text']}</p>
+                <button class="ellipsis-button" id="ellipsisButton" onclick="showRemainingText()">дальше</button>
+            </div>
+    `
+
+
+main.style.backgroundImage = "url(" + plot[i]['bgColor'] + ")"
+
+main.innerHTML = render
+
+
+document.querySelector('.top-next-button').onclick = () => {
+
+    
+    i++
+    main.style.backgroundImage = "url(" + plot[i]['bgColor'] + ")"
+    main.innerHTML = ''
+    main.innerHTML = `
+    ${plot[i]['leftImage'] ? "<div class='character' id='character-left' style='background-image: url(" + plot[i]['leftImage'] + ")'></div>" : ""}
+
+        
+        <div class="dialog-box" id="dialogBox">
+            <p><span class="name_span">${plot[i]['speaker'] ? plot[i]['speaker'] + ':' : ''}</span></p>
+            <p id="dialogText">${plot[i]['text']}</p>
+            <button class="ellipsis-button" id="ellipsisButton" onclick="showRemainingText()">дальше</button>
+        </div>
+
+    ${plot[i]['rightImage'] ? "<div class='character' id='character-right' style='background-image: url(" + plot[i]['rightImage'] + ")'></div>" : ""}
+` 
+
+// Обработка текста в диалоговом окне
+let dialogTextElement = document.getElementById('dialogText');
+let ellipsisButton = document.getElementById('ellipsisButton');
+let remainingText = dialogTextElement.textContent.slice(150);
+
+console.log(remainingText)
+
+// Показать оставшийся текст при клике на кнопку "..."
+window.showRemainingText = function () {
+    dialogTextElement.textContent = remainingText.slice(0, 150);
+    remainingText = remainingText.slice(150);
+
+    if (remainingText.length === 0) {
+        ellipsisButton.style.display = 'none';
+    }
+};
+
+// Показать следующий текст при клике на кнопку "Дальше"
+window.showNextText = function () {
+    if (remainingText.length > 0) {
+        dialogTextElement.textContent = remainingText.slice(0, 150);
+        remainingText = remainingText.slice(150);
+
+        if (remainingText.length > 0) {
+            ellipsisButton.style.display = 'inline-block';
+        } else {
+            ellipsisButton.style.display = 'none';
+        }
+    }
+};
+
+// Ограничение текста в диалоговом окне на 150 символов
+if (dialogTextElement.textContent.length > 150) {
+    ellipsisButton.style.display = 'inline-block';
+    dialogTextElement.textContent = dialogTextElement.textContent.slice(0, 150) + '...';
+}
+}
+
+document.querySelector('.top-back-button').onclick = () => {
+
+    
+    i--
+    main.style.backgroundImage = "url(" + plot[i]['bgColor'] + ")"
+    main.innerHTML = ''
+    main.innerHTML = `
+    ${plot[i]['leftImage'] ? "<div class='character' id='character-left' style='background-image: url(" + plot[i]['leftImage'] + ")'></div>" : ""}
+
+        
+        <div class="dialog-box" id="dialogBox">
+            <p><span class="name_span">${plot[i]['speaker'] ? plot[i]['speaker'] + ':' : ''}</span></p>
+            <p id="dialogText">${plot[i]['text']}</p>
+            <button class="ellipsis-button" id="ellipsisButton" onclick="showRemainingText()">дальше</button>
+        </div>
+
+    ${plot[i]['rightImage'] ? "<div class='character' id='character-right' style='background-image: url(" + plot[i]['rightImage'] + ")'></div>" : ""}
+` 
+
+// Обработка текста в диалоговом окне
+let dialogTextElement = document.getElementById('dialogText');
+let ellipsisButton = document.getElementById('ellipsisButton');
+let remainingText = dialogTextElement.textContent.slice(150);
+
+console.log(remainingText)
+
+// Показать оставшийся текст при клике на кнопку "..."
+window.showRemainingText = function () {
+    dialogTextElement.textContent = remainingText.slice(0, 150);
+    remainingText = remainingText.slice(150);
+
+    if (remainingText.length === 0) {
+        ellipsisButton.style.display = 'none';
+    }
+};
+
+// Показать следующий текст при клике на кнопку "Дальше"
+window.showNextText = function () {
+    if (remainingText.length > 0) {
+        dialogTextElement.textContent = remainingText.slice(0, 150);
+        remainingText = remainingText.slice(150);
+
+        if (remainingText.length > 0) {
+            ellipsisButton.style.display = 'inline-block';
+        } else {
+            ellipsisButton.style.display = 'none';
+        }
+    }
+};
+
+// Ограничение текста в диалоговом окне на 150 символов
+if (dialogTextElement.textContent.length > 150) {
+    ellipsisButton.style.display = 'inline-block';
+    dialogTextElement.textContent = dialogTextElement.textContent.slice(0, 150) + '...';
+}
+}
+
+
+// Обработка текста в диалоговом окне
+let dialogTextElement = document.getElementById('dialogText');
+let ellipsisButton = document.getElementById('ellipsisButton');
+let remainingText = dialogTextElement.textContent.slice(150);
+
+console.log(remainingText)
+
+// Показать оставшийся текст при клике на кнопку "..."
+window.showRemainingText = function () {
+    dialogTextElement.textContent = remainingText.slice(0, 150);
+    remainingText = remainingText.slice(150);
+
+    if (remainingText.length === 0) {
+        ellipsisButton.style.display = 'none';
+    }
+};
+
+// Показать следующий текст при клике на кнопку "Дальше"
+window.showNextText = function () {
+    if (remainingText.length > 0) {
+        dialogTextElement.textContent = remainingText.slice(0, 150);
+        remainingText = remainingText.slice(150);
+
+        if (remainingText.length > 0) {
+            ellipsisButton.style.display = 'inline-block';
+        } else {
+            ellipsisButton.style.display = 'none';
+        }
+    }
+};
+
+// Ограничение текста в диалоговом окне на 150 символов
+if (dialogTextElement.textContent.length > 150) {
+    ellipsisButton.style.display = 'inline-block';
+    dialogTextElement.textContent = dialogTextElement.textContent.slice(0, 150) + '...';
+}
+
+}()
